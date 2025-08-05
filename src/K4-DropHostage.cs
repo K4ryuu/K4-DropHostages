21deb37e1e30b75d135a9cbb98f23957b21da62f
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2025-08-05 15:44:35 +0200

    feat: Refactor plugin to use external gamedata file and update function signatures

--- src/K4-DropHostage.cs
+++ src/K4-DropHostage.cs
@@ -1,6 +1,9 @@
 using System.Text.Json.Serialization;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
 using CounterStrikeSharp.API.Modules.Utils;
 
@@ -12,55 +15,81 @@ public class Plugin : BasePlugin, IPluginConfig<PluginConfig>
 	public override string ModuleName => "CS2 Drop Hostages";
 	public override string ModuleAuthor => "K4ryuu @ KitsuneLab";
 	public override string ModuleDescription => "Drop hostages in CS2 using a command or drop weapon key.";
-	public override string ModuleVersion => "1.0.1";
+	public override string ModuleVersion => "1.1.0";
 
 	public PluginConfig Config { get; set; } = new();
 	public void OnConfigParsed(PluginConfig config) { this.Config = config; }
 
-	private static readonly MemoryFunctionVoid<CBaseEntity, Vector, bool> DropHostage
-		= new("55 48 89 E5 41 57 41 56 41 55 49 89 F5 41 54 41 89 D4 53 48 89 FB 48 81 EC 88 01 00 00"); // ! TODO: Add gamedata file for both Linux and Windows
+	private static readonly MemoryFunctionVoid<CBaseEntity, Vector, bool> CHostage_DropHostage
+		= new(GameData.GetSignature("CHostage_DropHostage"));
 
-	private readonly MemoryFunctionVoid<CCSPlayerPawn, CBasePlayerWeapon> DropWeapon
-		= new("55 48 89 E5 41 57 41 56 41 55 49 89 F5 41 54 53 48 89 FB 48 83 EC ? 48 8D 05 ? ? ? ? 48 8B 00");
+	private readonly MemoryFunctionVoid<CCSPlayerPawn, CBasePlayerWeapon> CCSPlayer_HandleDropWeapon
+		= new(GameData.GetSignature("CCSPlayer_HandleDropWeapon"));
+
+	private bool _pluginEnabled = false;
 
 	public override void Load(bool hotReload)
 	{
 		if (Config.HookDropButton)
-			DropWeapon.Hook(WeaponDrop_Hook, HookMode.Pre);
+			CCSPlayer_HandleDropWeapon.Hook(WeaponDrop_Hook, HookMode.Pre);
 
-		if (Config.RegisterCommand)
+		if (Config.Commands.Length != 0)
 		{
 			foreach (var command in Config.Commands)
 			{
 				var _command = command.StartsWith("css_") ? command : $"css_{command}";
-				AddCommand(_command, "Drop the hostage you're carrying.", (player, info) =>
-				{
-					if (player == null || !player.IsValid)
-						return;
-
-					var pawn = player.PlayerPawn.Value;
-					if (pawn == null || !pawn.IsValid)
-						return;
-
-					var hostage = pawn.HostageServices?.CarriedHostage.Value;
-					if (hostage == null || !hostage.IsValid)
-						return;
-
-					Vector dropPosition = GetPropPosition(pawn, 30);
-					DropHostage.Invoke(hostage, dropPosition, false);
-				});
+				AddCommand(_command, "Drop the hostage you're carrying.", OnDropHostageCommand);
 			}
 		}
+
+		if (hotReload)
+			CheckAndTogglePlugin();
 	}
 
 	public override void Unload(bool hotReload)
 	{
 		if (Config.HookDropButton)
-			DropWeapon.Unhook(WeaponDrop_Hook, HookMode.Pre);
+			CCSPlayer_HandleDropWeapon.Unhook(WeaponDrop_Hook, HookMode.Pre);
+	}
+
+	[GameEventHandler]
+	public HookResult Event_RoundStart(EventRoundStart @event, GameEventInfo info)
+	{
+		CheckAndTogglePlugin();
+		return HookResult.Continue;
+	}
+
+	private void CheckAndTogglePlugin()
+	{
+		var hostages = Utilities.FindAllEntitiesByDesignerName<CBaseEntity>("hostage_entity");
+		_pluginEnabled = hostages.Any();
+	}
+
+	private void OnDropHostageCommand(CCSPlayerController? player, CommandInfo info)
+	{
+		if (!_pluginEnabled)
+			return;
+
+		if (player == null || !player.IsValid)
+			return;
+
+		var pawn = player.PlayerPawn.Value;
+		if (pawn == null || !pawn.IsValid)
+			return;
+
+		var hostage = pawn.HostageServices?.CarriedHostage.Value;
+		if (hostage == null || !hostage.IsValid)
+			return;
+
+		Vector dropPosition = GetPropPosition(pawn, 30);
+		CHostage_DropHostage.Invoke(hostage, dropPosition, false);
 	}
 
 	private HookResult WeaponDrop_Hook(DynamicHook hook)
 	{
+		if (!_pluginEnabled)
+			return HookResult.Continue;
+
 		var pawn = hook.GetParam<CCSPlayerPawn>(0);
 		if (pawn == null || !pawn.IsValid)
 			return HookResult.Continue;
@@ -70,7 +99,7 @@ public class Plugin : BasePlugin, IPluginConfig<PluginConfig>
 			return HookResult.Continue;
 
 		Vector dropPosition = GetPropPosition(pawn, 30);
-		DropHostage.Invoke(hostage, dropPosition, false);
+		CHostage_DropHostage.Invoke(hostage, dropPosition, false);
 		return HookResult.Handled;
 	}
 
@@ -90,9 +119,6 @@ public class PluginConfig : BasePluginConfig
 {
 	[JsonPropertyName("HookDropButton")]
 	public bool HookDropButton { get; set; } = true;
-
-	[JsonPropertyName("RegisterCommand")]
-	public bool RegisterCommand { get; set; } = false;
 
 	[JsonPropertyName("Commands")]
 	public string[] Commands { get; set; } = ["drophostage", "dh"];
